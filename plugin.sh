102a6838c0c892c9600afeab1ee04ffcd017fa22
Myzel394 <50424412+Myzel394@users.noreply.github.com>
2024-06-21 09:21:25 +0200

    fix: Increase max wrap lines

--- plugin.sh
+++ plugin.sh
@@ -12,7 +12,7 @@ if [[ -f /tmp/.tmux-biff-buffer-check ]]; then
 
     mv /tmp/.tmux-biff-buffer-check /tmp/.tmux-biff-buffer-check-1
 
-    tmux new-window 'diff -u -U 9999 /tmp/.tmux-biff-buffer-check-1 /tmp/.tmux-biff-buffer-check-2 | delta --side-by-side; read -n 1'
+    tmux new-window 'diff -u -U 9999 /tmp/.tmux-biff-buffer-check-1 /tmp/.tmux-biff-buffer-check-2 | delta --side-by-side --wrap-max-lines 99; read -n 1'
 else
     echo $selected_text > /tmp/.tmux-biff-buffer-check
 fi
